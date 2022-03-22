bba882151d17513dc85c97d268c9828de9167116
ktresel2 <ktreseler2@gmail.com>
2022-03-22 18:07:39 -0400

    Set up app template

---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<div>
+			<h1>About this app</h1>
+			<p className="my-1">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam
+				accusamus id architecto facere nulla ipsam.
+			</p>
+			<p className="bg-dark p">
+				<strong>Version</strong>1.0.0
+			</p>
+		</div>
+	)
+}
+
+export default About
